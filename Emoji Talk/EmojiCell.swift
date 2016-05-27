3909d0646d14a2544d3aafac17c0b86b9d020bca
cablej <jackhcable@gmail.com>
2016-05-27 17:46:05 -0500

    Added emoji collection

---
+++ Emoji Talk/EmojiCell.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiCell.swift
+//  Emoji Talk
+//
+//  Created by Jack Cable on 5/27/16.
+//  Copyright © 2016 Uhack Emoji. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCell: UICollectionViewCell {
+
+    @IBOutlet var emojiLabel: UILabel!
+    
+}
--- Emoji Talk/ViewController.swift
+++ Emoji Talk/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import EmojiKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet var emojiCollectionView: UICollectionView!
+    
+    var emojiList : [Emoji] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fetcher = EmojiFetcher()
+        
+        emojiCollectionView.dataSource = self
+        emojiCollectionView.delegate = self
+        
+        fetcher.query("face") { emojiResults in
+            /*for emoji in emojiResults {
+                print("Current Emoji: \(emoji.character) \(emoji.name)")
+            }*/
+            self.emojiList = emojiResults
+            dispatch_async(dispatch_get_main_queue(),{ //operate on main thread
+                self.emojiCollectionView.reloadData()
+            })
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return emojiList.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = emojiCollectionView.dequeueReusableCellWithReuseIdentifier("EmojiCell", forIndexPath: indexPath) as! EmojiCell
+        
+        cell.emojiLabel.text = emojiList[indexPath.row].character
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
