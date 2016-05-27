245bb128fd10e1bdc4391310fb14248c201213e3
cablej <jackhcable@gmail.com>
2016-05-27 17:07:39 -0500

    Initial Commit

---
+++ Emoji Talk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Talk
+//
+//  Created by Jack Cable on 5/27/16.
+//  Copyright © 2016 Uhack Emoji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
